2a0b088e8bc2d04e2c31bb8e7ab072612fb008fa
Christos Psarras <psarras@aices.rwth-aachen.de>
2018-10-09 00:23:04 +0200

    Change deprecated immutable to struct

--- src/Properties.jl
+++ src/Properties.jl
@@ -1,43 +1,37 @@
-
-
 module Properties
 
-  export Random, Positive, Negative
+  export Random, Constant, Positive, Negative, Symmetric, SPD, Orthogonal
 
-  immutable Random
+  struct Random
     lower_bound::Float64
     upper_bound::Float64
 
     function Random(lower_bound, upper_bound)
       if lower_bound > upper_bound
-        throw(ErrorException( @sprintf("Incorrect random boundaries: lower bound %f
-            greater than upper bound %f", lower_bound,upper_bound) ))
+        throw(ErrorException("Incorrect random boundaries: lower bound
+                             $lower_bound greater than upper bound $upper_bound"))
       end
       return new(lower_bound, upper_bound)
     end
   end
 
-  immutable Positive
-  end
-
-  immutable Negative
-  end
-
-  immutable Symmetric
-  end
-
-  immutable Constant
+  struct Constant
     value::Float64
 
+    #TODO examine if default to 0 is a good idea
     function Constant(value::Float64 = 0.0)
       return new(value)
     end
   end
 
-  immutable SPD
-  end
+  struct Positive end
 
-  immutable Orthogonal
-  end
+  struct Negative end
+
+  struct Symmetric end
+
+  struct SPD end
+
+  struct Orthogonal end
 
 end
