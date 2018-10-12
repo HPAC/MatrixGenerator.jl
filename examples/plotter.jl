579b4144cc08376ac62936f8110f70788c1e128f
Christos Psarras <psarras@aices.rwth-aachen.de>
2018-10-12 18:07:59 +0200

    Fix Plotter example

--- examples/plotter.jl
+++ examples/plotter.jl
@@ -8,11 +8,11 @@ end
 
 plotter = Benchmarker.Plot("julia_data.txt", ["N"; "N^2"])
 for i=50:10:100
-  Benchmarkeradd_data(plotter, [i; i^2], @Benchmarker.time(matmul(i)))
+  Benchmarker.add_data(plotter, [i; i^2], @Benchmarker.time(matmul(i)))
 end
 Benchmarker.finish(plotter);
 
-plotter2 = Benchmarker.Plot{Float64}("julia_data2.txt")
+plotter2 = Benchmarker.Plot("julia_data2.txt")
 for i=50:10:100
   Benchmarker.add_data(plotter2, @Benchmarker.time(matmul(i)))
 end
