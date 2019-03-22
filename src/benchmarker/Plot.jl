a3721e92be90e9e9dc9ce465f1da7fc71a897efa
Henrik Barthels <barthels@aices.rwth-aachen.de>
2019-03-22 11:08:40 +0100

    Changed output files.

--- src/benchmarker/Plot.jl
+++ src/benchmarker/Plot.jl
@@ -12,10 +12,10 @@ function Plot(name::String, extra_labels::Array{String, 1} = Array{String, 1}(),
   labels = ["Time" "StdDev" "Min" "Max"]
   labels = isempty(extra_labels) ? labels : [reshape(extra_labels, (1, :)) labels]
 
-  f = open(name, "w")
+  f = open(string(name,".txt"), "w")
   writedlm(f, labels, delimiter)
 
-  ft = open(string(name,".timings"), "w")
+  ft = open(string(name,"_timings.txt"), "w")
   return Plot(f, ft, delimiter)
 end
 
@@ -24,7 +24,9 @@ function add_data(p::Plot, extra_data::Array{P, 1}, timings::Results) where P
   data = isempty(extra_data) ? t : [reshape(extra_data, (1, :)) t]
   writedlm(p.file, data, p.delimiter)
 
-  writedlm(p.file_timings, transpose(timings.timings), p.delimiter)
+  t = transpose(timings.timings)
+  data = isempty(extra_data) ? t : [reshape(extra_data, (1, :)) t]
+  writedlm(p.file_timings, data, p.delimiter)
 end
 
 function finish(p::Plot)
